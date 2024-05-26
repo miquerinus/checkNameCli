2a7c0da7869a7fc742e0297ccaa9cb31d86d8d47
miquerinus <miquerinus.br@gmail.com>
2024-05-25 22:08:40 -0300

    update commit for repository

---
+++ README.md
@@ -0,0 +1,10 @@
+<h1>  Check name cli</h1>
+
+<h2> Sobre: </h2>
+- é um pequeno fragmento de algoritmo escrito em C#
+- tem por finalidade checar a entrada de 2 dados de tipo String
+- tem funções que avaliam cada entrada.
+- tipo de programa é para console.
+
+<h2> Conclusão: </h2>
+- embora simples o algoritmo trata de conceitos importantes.
---
+++ ex24/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Algoritmo abaixo tem a finadalide de comparar o conteúdo de 2 variaveis se são iguais ou não. Caso o conteudo for igual o retorno sera TRUE, caso os valores forem diferentes
+o retorno sera FALSE. */
+using System;
+
+namespace Names 
+{
+
+    public class checkNames()
+    {
+        public static bool verificar(string n1, string n2)
+        {
+            return n1 == n2;
+        }
+    }
+}
+
+class Program
+{
+   static void Main(string[] args)
+   {
+     Console.WriteLine("insira alguma frase: ");
+     string input1 = Console.ReadLine();
+
+     Console.WriteLine();
+
+     Console.WriteLine("insira uma nova frase: ");
+     string input2 = Console.ReadLine();
+
+     bool result = Names.checkNames.verificar(input1, input2);
+
+     Console.WriteLine($"-> As letras informadas são iguais: {result} "); 
+   }
+}
